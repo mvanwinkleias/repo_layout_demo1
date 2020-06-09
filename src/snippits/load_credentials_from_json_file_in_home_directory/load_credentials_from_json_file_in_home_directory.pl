45356fccb0ebc66b1a72ff265ac6dcc41026c745
Martin VanWinkle III <mvanwinkle@ias.edu>
2020-06-08 20:13:37 -0400

    spaces to tabs...

--- src/snippits/load_credentials_from_json_file_in_home_directory/load_credentials_from_json_file_in_home_directory.pl
+++ src/snippits/load_credentials_from_json_file_in_home_directory/load_credentials_from_json_file_in_home_directory.pl
@@ -8,31 +8,31 @@ use Data::Dumper;
 use Getopt::Long;
 
 my $OPTIONS_VALUES = {
-    'ipam-credentials-file' => glob('~/.config/IAS/some-awesome-project/ipam_credentials.json'),
+	'ipam-credentials-file' => glob('~/.config/IAS/some-awesome-project/ipam_credentials.json'),
 };
 
 my @OPTIONS = (
-    'ipam-credentials-file=s'
+	'ipam-credentials-file=s'
 );
 
 GetOptions(
-    $OPTIONS_VALUES,
-    @OPTIONS,
+	$OPTIONS_VALUES,
+	@OPTIONS,
 )
 or die "Bad options.\n";
 
 print Dumper(load_json_file($OPTIONS_VALUES->{'ipam-credentials-file'}));
 sub load_json_file
 {
-    my ($filename) = @_;
-    use IO::File;
-    my $fh = IO::File->new($filename)
-        or die "Can't open $filename for reading: $!";
+	my ($filename) = @_;
+	use IO::File;
+	my $fh = IO::File->new($filename)
+		or die "Can't open $filename for reading: $!";
 	my $contents;
-    {
-        local $/;
-        $contents = <$fh>;
-    }
+	{
+		local $/;
+		$contents = <$fh>;
+	}
 	$fh->close();
-    return JSON::decode_json($contents);
+	return JSON::decode_json($contents);
 }
