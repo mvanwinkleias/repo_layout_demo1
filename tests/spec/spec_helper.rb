6fffe59e114149c5960033a6067a6c8c3efe7ba0
Martin VanWinkle III <mvanwinkle@ias.edu>
2020-10-19 09:09:02 -0400

    putting things under tests

--- tests/spec/spec_helper.rb
+++ tests/spec/spec_helper.rb
@@ -3,7 +3,7 @@ if File.lstat(realbin).symlink?
         realbin=File.readlink(__FILE__)
 end
 
-project_lib_dir = File.expand_path('../src/lib/ruby2',File.dirname(realbin))
+project_lib_dir = File.expand_path('../../src/lib/ruby2',File.dirname(realbin))
 
 $:.unshift('/opt/IAS/lib/ruby2')
 $:.unshift(File.expand_path(project_lib_dir,File.dirname(realbin)))
