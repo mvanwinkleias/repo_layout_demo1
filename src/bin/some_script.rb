54970eaeac70518e1dfe2592b8a4de224405aeaf
Martin VanWinkle III <mvanwinkle@ias.edu>
2021-11-28 08:01:25 -0500

    rubocop stuff...

--- src/bin/some_script.rb
+++ src/bin/some_script.rb
@@ -1,15 +1,15 @@
 #!/usr/bin/ruby
 
-realbin=__FILE__
+realbin = __FILE__
 if File.lstat(realbin).symlink?
-	realbin=File.readlink(__FILE__)
+  realbin = File.readlink(__FILE__)
 end
 
-$:.unshift('/opt/IAS/lib/ruby2')
-$:.unshift(File.expand_path('../lib/ruby2/',File.dirname(realbin)))
+$LOAD_PATH.unshift('/opt/IAS/lib/ruby2')
+$LOAD_PATH.unshift(File.expand_path('../lib/ruby2/', File.dirname(realbin)))
 
 require 'ias/hello_repo_layout'
 
 app = IAS::HelloRepoLayout.new
 
-app.hello()
+app.hello
--- src/lib/ruby2/ias/hello_repo_layout.rb
+++ src/lib/ruby2/ias/hello_repo_layout.rb
@@ -1,13 +1,13 @@
 module IAS; end
 
 class IAS::HelloRepoLayout
-	# This is an instance method
-	def hello
-		puts "Hello, IAS Repo!\n"
-	end
-	
-	# This is a class method
-	def self.simple_return
-		return 1
-	end
+  # This is an instance method
+  def hello
+    puts "Hello, IAS Repo!\n"
+  end
+
+  # This is a class method
+  def self.simple_return
+    return 1
+  end
 end
