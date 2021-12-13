09999f88f041c30f1a44649fb10819e36da1575a
Martin VanWinkle III <mvanwinkle@ias.edu>
2021-12-12 22:08:54 -0500

    shortening things

--- src/lib/python3/ias/hello_repo_layout/__init__.py
+++ src/lib/python3/ias/hello_repo_layout/__init__.py
@@ -1,1 +1,12 @@
-from ias.hello_repo_layout.hello_repo_layout import *
+# Here's how you'd specify the full thing:
+# from ias.hello_repo_layout.hello_repo_layout import *
+# Here's how you'd specify the relative thing:
+from .hello_repo_layout import *
+
+
+# This might not be possible below; it would be nice to be
+# able to:
+# import os
+# import importlib
+# __module_name=os.path.split(__path__[0])[-1]
+# from '.'+__module_name import *
