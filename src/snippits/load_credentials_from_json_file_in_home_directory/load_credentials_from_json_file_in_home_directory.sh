64804950d17a600db29ccead6a834c9b36b09899
Martin VanWinkle III <mvanwinkle@ias.edu>
2021-12-26 20:37:09 -0500

    actual working example.

--- src/snippits/load_credentials_from_json_file_in_home_directory/load_credentials_from_json_file_in_home_directory.sh
+++ src/snippits/load_credentials_from_json_file_in_home_directory/load_credentials_from_json_file_in_home_directory.sh
@@ -4,7 +4,9 @@
 # making some of your bash stuff require the usage
 # of jq to get data out of it.
 
-$json_credentials_file="$HOME/.config/IAS/some-awesome-project/ipam_credentials.json"
+json_credentials_file=${json_credentials_file:-$HOME/.config/IAS/some-awesome-project/ipam_credentials.json}
 
-username=`$jq --raw-output '.username' "$json_credentials_file"`
-password=`$jq --raw-output '.password' "$json_credentials_file"`
+username=$(jq --raw-output '.username' "$json_credentials_file")
+password=$(jq --raw-output '.password' "$json_credentials_file")
+
+printf "Username: %s\n" "$username"
