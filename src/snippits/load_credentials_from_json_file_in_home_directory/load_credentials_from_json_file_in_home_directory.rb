b4403d21dd5808623d118c0b928e767e67e7841b
Martin VanWinkle III <mvanwinkle@ias.edu>
2022-07-11 14:05:56 -0400

    progress

--- src/snippits/load_credentials_from_json_file_in_home_directory/load_credentials_from_json_file_in_home_directory.rb
+++ src/snippits/load_credentials_from_json_file_in_home_directory/load_credentials_from_json_file_in_home_directory.rb
@@ -1,13 +1,26 @@
 #!/usr/bin/ruby
 
 require 'json'
+require 'getoptlong'
+require 'pp'
 
-data=JSON.parse(
-  File.read(
-    ENV['HOME'] \
-    + '/.config/IAS/some-awesome-project/ipam_credentials.json'
-  )
+opts = GetoptLong.new(
+  [ '--ipam-credentials-file', GetoptLong::OPTIONAL_ARGUMENT ],
 )
 
+# pp(opts)
+
+options = {
+  'ipam-credentials-file' => File.expand_path('~/.config/IAS/some-awesome-project/ipam_credentials.json')
+}
+
+opts.each do |opt, arg|
+  case opt
+    when '--ipam-credentials-file'
+      options['ipam-credentials-file'] = arg
+  end
+end
+
+data=JSON.parse(File.read(options['ipam-credentials-file']))
 print 'Username: ' + data['username'] + "\n"
 
