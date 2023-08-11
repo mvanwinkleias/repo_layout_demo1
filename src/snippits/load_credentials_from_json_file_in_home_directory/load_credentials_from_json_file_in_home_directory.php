3803d4d232b54173f32a0c44d1dbd924a2ffe969
Martin VanWinkle III <mvanwinkle@ias.edu>
2023-08-11 12:25:24 -0400

    progress

--- src/snippits/load_credentials_from_json_file_in_home_directory/load_credentials_from_json_file_in_home_directory.php
+++ src/snippits/load_credentials_from_json_file_in_home_directory/load_credentials_from_json_file_in_home_directory.php
@@ -4,7 +4,6 @@
 // Command line options:
 //     --ipam-credentials-file='/some/other/path.json'
 
-// COPY/PASTE (doesn't change)
 function loadJSONFromFile($filename)
 {
     $contents = file_get_contents($filename);
@@ -13,9 +12,7 @@ function loadJSONFromFile($filename)
 }
 
 $my_home_dir = posix_getpwuid(posix_getuid())['dir'];
-// End COPY/PASTE
 
-// Boilerplate code for options
 $shortopts = "";
 $longopts = array(
     'ipam-credentials-file::', // This is optional
@@ -24,12 +21,9 @@ $default_options = array(
     'ipam-credentials-file' => "$my_home_dir/.config/IAS/some-awesome-project/ipam_credentials.json",
 );
 
-// Addition of associative arrays in PHP is UNION with left side
-// preferred
 $options = getopt($shortopts, $longopts) + $default_options;
 
 $ipam_credentials = loadJSONFromFile($options['ipam-credentials-file']);
-// End Boilerplate code for options
 
 print("Options:\n");
 var_dump($options);
