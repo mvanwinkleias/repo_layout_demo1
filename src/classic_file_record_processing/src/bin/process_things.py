9bbdfd9926c40c201687c1b9b7ddf7b598f78c10
Martin VanWinkle III <mvanwinkle@ias.edu>
2025-02-12 11:45:49 -0500

    progress

--- src/classic_file_record_processing/src/bin/process_things.py
+++ src/classic_file_record_processing/src/bin/process_things.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
 import argparse
+import sys
 import os
 import pprint
+import json
 
 class Processor1:
     argument_parser = {
@@ -21,9 +23,13 @@ class Processor1:
             action="store_true",
         )
 
+        arg_parser.add_argument(
+            'files',
+            type=argparse.FileType('r'),
+            nargs="*",
+        )
+
         self.args = arg_parser.parse_args()        
-
-
 
     def run( self ):
         # print("Hello, self")
@@ -33,6 +39,24 @@ class Processor1:
             pp = pprint.PrettyPrinter(indent=4)
             pp.pprint(self.__dict__)
             exit()
+
+        self.process_file_handles()
+
+    def process_file_handles( self ):
+        # pprint.pprint(self.args.files)
+        if (len(self.args.files) > 0):
+            for file in self.args.files :
+                for line in file:
+                    self.process_record(
+                        self.decode_line(line)
+                    )
+        
+
+    def decode_line( self, line ):
+        return json.loads(line)
+    
+    def process_record( self, record ) :
+        pprint.pprint(record)
 
 
 if __name__ == '__main__':
