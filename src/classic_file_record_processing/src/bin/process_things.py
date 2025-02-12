435b30dd185b86b88e2453506307d56d922a664b
Martin VanWinkle III <mvanwinkle@ias.edu>
2025-02-12 11:34:02 -0500

    progress

--- src/classic_file_record_processing/src/bin/process_things.py
+++ src/classic_file_record_processing/src/bin/process_things.py
@@ -8,43 +8,31 @@ class Processor1:
     argument_parser = {
         'prog' : os.path.basename(__file__),
         'description' : 'This processes things.',
-        'epilog' : 'Why do things like this?',
-    }
-    
-
-    arguments = {
-        'dump-self' : { 'action':'store_true' },
-        'some-config-file' : {
-            'type' : os.path.expanduser,
-            'default' : '~/.config/some-config-file.json',
-            'help' : 'Path to some config file.'
-        },
+        'epilog' : 'This is an epilog.',
     }
 
     def load_arguments( self ):
-        parser = argparse.ArgumentParser(
+        arg_parser = argparse.ArgumentParser(
             ** self.argument_parser
         )
 
-        for argument_name in self.arguments:
-            arg_dict = self.arguments[argument_name]
-            arg_dict['dest'] = argument_name
-            parser.add_argument(
-                '--' + argument_name,
-                **arg_dict,
-            )
+        arg_parser.add_argument(
+            "--dump-self",
+            action="store_true",
+        )
 
-        return parser
+        self.args = arg_parser.parse_args()        
+
 
 
     def run( self ):
         # print("Hello, self")
-        self.argparse = self.load_arguments()
-        self.argument_values = vars(self.argparse.parse_args())
+        self.load_arguments()
 
-        if (self.argument_values['dump-self']):
+        if (self.args.dump_self):
             pp = pprint.PrettyPrinter(indent=4)
             pp.pprint(self.__dict__)
+            exit()
 
 
 if __name__ == '__main__':
